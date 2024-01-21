a37c7999ffeae5b009b07b25326900df0c8d5134
Md. Parvez <mdparvezhasan2672@gmail.com>
2024-01-21 13:43:07 +0600

    Day52 React

---
+++ ait-restaurent/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MainComponent from './MainComponent'
+
+function App() {
+
+  return (
+    <div>
+       <MainComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ ait-restaurent/src/MainComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./mainComponent/body/Body";
+import Header from "./mainComponent/header/Header";
+import Footer from "./mainComponent/footer/Footer";
+
+const MainComponent= () =>{
+
+    return (
+      <>
+        <Header/>
+        
+      </>
+    )
+  }
+  export default MainComponent;
+  
